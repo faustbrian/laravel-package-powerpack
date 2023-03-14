0503901aeefacf43fabc833f0452e30ca66ba664
Brian Faust <hello@basecode.sh>
2023-03-14 14:55:43 +0200

    refactor: parse `composer.json` via `json_decode`

--- src/Package/Concerns/HasComposerJson.php
+++ src/Package/Concerns/HasComposerJson.php
@@ -4,26 +4,24 @@ declare(strict_types=1);
 
 namespace PreemStudio\Jetpack\Package\Concerns;
 
-use PreemStudio\ComposerParser\Package as Composer;
-use PreemStudio\ComposerParser\PackageFactory;
 use PreemStudio\Jetpack\Package\Package;
 use RuntimeException;
 
 trait HasComposerJson
 {
-    protected function getPackageManifest(Package $package): Composer
+    protected function getPackageManifest(Package $package): array
     {
-        return PackageFactory::fromPath($package->basePath('..'));
+        return json_decode(file_get_contents(realpath($package->rootPath('composer.json'))), true, 512, JSON_THROW_ON_ERROR);
     }
 
     protected function getPackageName(Package $package): string
     {
-        return explode('/', $this->getPackageManifest($package)->name)[1];
+        return explode('/', $this->getPackageManifest($package)['name'])[1];
     }
 
     protected function getPackageNamespace(Package $package): string
     {
-        $namespace = $this->getPackageManifest($package)->autoload?->psr_4;
+        $namespace = $this->getPackageManifest($package)['autoload']['psr-4'];
 
         if (is_array($namespace)) {
             return (string) array_key_first($namespace);
