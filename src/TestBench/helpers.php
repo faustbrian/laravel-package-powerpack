b893e0e644738723bdc5d5c090b31a20e554c923
Brian Faust <hello@basecode.sh>
2023-03-17 13:28:58 +0200

    fix: only register pest helpers if pest is installed

--- src/TestBench/helpers.php
+++ src/TestBench/helpers.php
@@ -6,96 +6,98 @@ use Illuminate\Support\Facades\Facade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Str;
 
-// Generic
-expect()->extend('toBeSubclassOf', function (string $class) {
-    $reflection = new ReflectionClass($this->value);
-    $provider   = new ReflectionClass($class);
+if (function_exists('expect')) {
+    // Generic
+    expect()->extend('toBeSubclassOf', function (string $class) {
+        $reflection = new ReflectionClass($this->value);
+        $provider   = new ReflectionClass($class);
 
-    expect($reflection->isSubclassOf($provider))->toBeTrue();
-});
+        expect($reflection->isSubclassOf($provider))->toBeTrue();
+    });
 
-// Service Provider
-expect()->extend('toBeServiceProvider', function () {
-    expect($this->value)->toBeSubclassOf(ServiceProvider::class);
-});
+    // Service Provider
+    expect()->extend('toBeServiceProvider', function () {
+        expect($this->value)->toBeSubclassOf(ServiceProvider::class);
+    });
 
-expect()->extend('toBeServiceProviderWithServices', function () {
-    $class      = $this->value;
-    $reflection = new ReflectionClass($class);
+    expect()->extend('toBeServiceProviderWithServices', function () {
+        $class      = $this->value;
+        $reflection = new ReflectionClass($class);
 
-    $method = $reflection->getMethod('provides');
-    $method->setAccessible(true);
+        $method = $reflection->getMethod('provides');
+        $method->setAccessible(true);
 
-    expect($method->invoke(new $class($this->app)))->toBeArray();
-});
+        expect($method->invoke(new $class($this->app)))->toBeArray();
+    });
 
-// Facade
-expect()->extend('toBeFacade', function (string $accessor, string $root) {
-    expect($this->value)->toMatchFacadeSubclass();
-    expect($this->value)->toMatchFacadeAccessor($accessor);
-    expect($this->value)->toMatchFacadeRoot($root);
-});
+    // Facade
+    expect()->extend('toBeFacade', function (string $accessor, string $root) {
+        expect($this->value)->toMatchFacadeSubclass();
+        expect($this->value)->toMatchFacadeAccessor($accessor);
+        expect($this->value)->toMatchFacadeRoot($root);
+    });
 
-expect()->extend('toMatchFacadeSubclass', function () {
-    expect($this->value)->toBeSubclassOf(Facade::class);
-});
+    expect()->extend('toMatchFacadeSubclass', function () {
+        expect($this->value)->toBeSubclassOf(Facade::class);
+    });
 
-expect()->extend('toMatchFacadeAccessor', function (string $accessor) {
-    $reflection = new ReflectionClass($this->value);
-    $method     = $reflection->getMethod('getFacadeAccessor');
-    $method->setAccessible(true);
+    expect()->extend('toMatchFacadeAccessor', function (string $accessor) {
+        $reflection = new ReflectionClass($this->value);
+        $method     = $reflection->getMethod('getFacadeAccessor');
+        $method->setAccessible(true);
 
-    expect($method->invoke(null))->toEqual($accessor);
-});
+        expect($method->invoke(null))->toEqual($accessor);
+    });
 
-expect()->extend('toMatchFacadeRoot', function (string $root) {
-    $reflection = new ReflectionClass($this->value);
-    $method     = $reflection->getMethod('getFacadeRoot');
-    $method->setAccessible(true);
+    expect()->extend('toMatchFacadeRoot', function (string $root) {
+        $reflection = new ReflectionClass($this->value);
+        $method     = $reflection->getMethod('getFacadeRoot');
+        $method->setAccessible(true);
 
-    expect($method->invoke(null))->toBeInstanceOf($root);
-});
+        expect($method->invoke(null))->toBeInstanceOf($root);
+    });
 
-expect()->extend('toBeFacadeProvider', function (string $accessor) {
-    $provider   = $this->value;
-    $reflection = new ReflectionClass($provider);
-    $method     = $reflection->getMethod('provides');
-    $method->setAccessible(true);
+    expect()->extend('toBeFacadeProvider', function (string $accessor) {
+        $provider   = $this->value;
+        $reflection = new ReflectionClass($provider);
+        $method     = $reflection->getMethod('provides');
+        $method->setAccessible(true);
 
-    expect($method->invoke(new $provider($this->app)))->toContain($accessor);
-});
+        expect($method->invoke(new $provider($this->app)))->toContain($accessor);
+    });
 
-// Container
-expect()->extend('toBeInjectable', function () {
-    $name       = $this->value;
-    $injectable = true;
+    // Container
+    expect()->extend('toBeInjectable', function () {
+        $name       = $this->value;
+        $injectable = true;
 
-    try {
-        do {
-            $class = 'testBenchStub'.Str::random();
-        } while (class_exists($class));
+        try {
+            do {
+                $class = 'testBenchStub'.Str::random();
+            } while (class_exists($class));
 
-        eval("
-            class $class
-            {
-                protected readonly mixed \$object;
-
-                public function __construct(\\$name \$object)
+            eval("
+                class $class
                 {
-                    \$this->object = \$object;
+                    protected readonly mixed \$object;
+
+                    public function __construct(\\$name \$object)
+                    {
+                        \$this->object = \$object;
+                    }
+
+                    public function getInjectedObject(): mixed
+                    {
+                        return \$this->object;
+                    }
                 }
+            ");
 
-                public function getInjectedObject(): mixed
-                {
-                    return \$this->object;
-                }
-            }
-        ");
+            expect(app()->make($class)->getInjectedObject())->toBeInstanceOf($name);
+        } catch (Exception) {
+            $injectable = false;
+        }
 
-        expect(app()->make($class)->getInjectedObject())->toBeInstanceOf($name);
-    } catch (Exception) {
-        $injectable = false;
-    }
-
-    expect($injectable)->toBeTrue();
-});
+        expect($injectable)->toBeTrue();
+    });
+}
